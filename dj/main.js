fcb7548f8756b486aebd3915eb44cc4f4af72dd3
Chaudhary-shruti <93135302+Chaudhary-shruti@users.noreply.github.com>
2023-02-03 23:39:26 -0800

    Add files via upload

---
+++ dj/main.js
@@ -0,0 +1,11 @@
+function setup(){
+    canvas=createCanvas(600,500)
+    canvas.position(400,150)
+    video=createCapture(VIDEO)
+    video.hide()}
+    function draw(){
+
+    }
+    function preload(){
+
+    }
